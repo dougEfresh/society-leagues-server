34b5a0f58e9bed3867df14adc303eb904c15dec3
Bob Smith <bobsmith@gmail.com>
2016-02-27 14:56:59 -0500

    fixing refresh results

---
+++ src/main/java/com/society/leagues/conf/spring/ListenerConfig.java
@@ -0,0 +1,17 @@
+package com.society.leagues.conf.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+
+@Component
+@SuppressWarnings("unused")
+public class ListenerConfig {
+
+    @Bean
+    ThreadPoolTaskExecutor threadPoolExecutor() {
+        ThreadPoolTaskExecutor threadPoolExecutor = new ThreadPoolTaskExecutor();
+        threadPoolExecutor.setCorePoolSize(10);
+        return threadPoolExecutor;
+    }
+}
