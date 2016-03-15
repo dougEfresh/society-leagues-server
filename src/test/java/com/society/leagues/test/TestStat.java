1912677b60a2534671b40dc0fdd74965b93c282a
Bob Smith <bobsmith@gmail.com>
2016-03-15 18:20:59 -0400

    stats

--- src/test/java/com/society/leagues/test/TestStat.java
+++ src/test/java/com/society/leagues/test/TestStat.java
@@ -38,11 +38,10 @@ public class TestStat  extends BaseTest {
 
     @Test
     public void bTeamStat() {
-        for (Season season : seasonApi.active()) {
+        for (Season season : seasonApi.active().stream().filter(s->!s.isScramble()).collect(Collectors.toList())) {
             User user = userApi.active().stream().filter(u -> u.hasSeason(season)).findFirst().get();
-            List<Stat> oldStats = statApi.getUserSeasonStats(user.getId(),season.getId());
             Team team = teamApi.seasonTeams(season.getId()).stream().filter(t -> t.hasUser(user)).findFirst().get();
-            List<TeamMatch> matches  = new ArrayList<>();
+            List<TeamMatch> matches = new ArrayList<>();
             if (season.isChallenge()) {
                 for (int i = 0; i<=1 ; i++) {
                     TeamMatch tm = teamMatchApi.add(season.getId(), LocalDate.now().toString());
@@ -53,10 +52,6 @@ public class TestStat  extends BaseTest {
             } else {
                 seasonApi.schedule(season.getId());
                  matches = teamMatchApi.matchesBySeasonList(season.getId()).stream().filter(t -> t.hasTeam(team)).limit(2).collect(Collectors.toList());
-                if (season.isScramble()) {
-                    matches.get(0).setDivision(Division.MIXED_EIGHT);
-                    matches.get(1).setDivision(Division.MIXED_NINE);
-                }
                 teamMatchApi.save(matches);
             }
             PlayerResult result = playerResultApi.add(matches.get(0).getId()).get(0);
@@ -88,20 +83,11 @@ public class TestStat  extends BaseTest {
             playerResultApi.save(Collections.singletonList(result));
 
             List<Stat> stats = statApi.getUserSeasonStats(user.getId(),season.getId());
-            if (season.isScramble()) {
-                //assertTrue(stats.stream().filter(s->s.getType() == StatType.ALL).count() == 1);
-                assertTrue(stats.stream().filter(s->s.getType() == StatType.USER_SEASON).count() == 1);
-                assertTrue(stats.stream().filter(s->s.getType() == StatType.MIXED_EIGHT).count() == 1);
-                assertTrue(stats.stream().filter(s->s.getType() == StatType.MIXED_NINE).count() == 1);
-                assertTrue(stats.stream().filter(s->s.getType() == StatType.MIXED_SCOTCH).count() == 1);
-            } else {
-                //assertTrue(stats.stream().filter(s->s.getType() == StatType.ALL).count() == 1);
-                assertTrue(stats.stream().filter(s->s.getType() == StatType.USER_SEASON).count() == 1);
-                assertTrue(stats.stream().filter(s->s.getType() == StatType.MIXED_EIGHT).count() == 0);
-                assertTrue(stats.stream().filter(s->s.getType() == StatType.MIXED_NINE).count() == 0);
-                assertTrue(stats.stream().filter(s->s.getType() == StatType.MIXED_SCOTCH).count() == 0);
-            }
-
+            //assertTrue(stats.stream().filter(s->s.getType() == StatType.ALL).count() == 1);
+            assertTrue(stats.stream().filter(s->s.getType() == StatType.USER_SEASON).count() == 1);
+            assertTrue(stats.stream().filter(s->s.getType() == StatType.MIXED_EIGHT).count() == 0);
+            assertTrue(stats.stream().filter(s->s.getType() == StatType.MIXED_NINE).count() == 0);
+            assertTrue(stats.stream().filter(s->s.getType() == StatType.MIXED_SCOTCH).count() == 0);
             for (Stat stat : stats) {
                 if (stat.getType() == StatType.USER_SEASON) {
                     assertEquals(new Integer(2),stat.getMatches());
@@ -126,15 +112,78 @@ public class TestStat  extends BaseTest {
                     assertEquals(new Integer(5),stat.getRacksLost());
                     assertEquals(new Integer(1),stat.getLoses());
                 }
-
-                if (season.isScramble() && stat.getType() == StatType.MIXED_SCOTCH) {
-                    assertEquals(new Integer(1),stat.getMatches());
-                    assertEquals(new Integer(0),stat.getWins());
-                    assertEquals(new Integer(1),stat.getLoses());
-                    assertEquals(new Integer(1),stat.getRacksWon());
-                    assertEquals(new Integer(5),stat.getRacksLost());
-                }
             }
         }
+    }
+    @Test
+    public void cTeamStat() {
+        Season season = seasonApi.active().stream().filter(s->s.isScramble()).findFirst().get();
+        User user = userApi.active().stream().filter(u -> u.hasSeason(season)).findFirst().get();
+        Team team = teamApi.seasonTeams(season.getId()).stream().filter(t -> t.hasUser(user)).findFirst().get();
+        List<TeamMatch> matches = new ArrayList<>();
+        seasonApi.schedule(season.getId());
+        matches = teamMatchApi.matchesBySeasonList(season.getId()).stream().filter(t -> t.hasTeam(team)).limit(2).collect(Collectors.toList());
+        matches.get(0).setDivision(Division.MIXED_EIGHT);
+        matches.get(1).setDivision(Division.MIXED_NINE);
+        teamMatchApi.save(matches);
+
+         PlayerResult result = playerResultApi.add(matches.get(0).getId()).get(0);
+        if (matches.get(0).getHome().equals(team)) {
+            result.setPlayerHome(user);
+            result.setHomeRacks(5);
+                result.setAwayRacks(1);
+        } else {
+            result.setPlayerAway(user);
+            result.setAwayRacks(5);
+            result.setHomeRacks(1);
+        }
+        playerResultApi.save(Collections.singletonList(result));
+        result = playerResultApi.add(matches.get(1).getId()).get(0);
+        if (matches.get(1).getHome().equals(team)) {
+            result.setPlayerHome(user);
+            result.setHomeRacks(1);
+            result.setAwayRacks(5);
+            result.setPlayerHomePartner(teamApi.members(team.getId()).getMembers().stream().filter(u->!u.equals(user)).findAny().get());
+        } else {
+            result.setPlayerAway(user);
+            result.setAwayRacks(1);
+            result.setHomeRacks(5);
+            result.setPlayerAwayPartner(teamApi.members(team.getId()).getMembers().stream().filter(u->!u.equals(user)).findAny().get());
+        }
+        playerResultApi.save(Collections.singletonList(result));
+        List<Stat> stats = statApi.getUserSeasonStats(user.getId(),season.getId());
+        assertTrue(stats.stream().filter(s->s.getType() == StatType.USER_SEASON).count() == 1);
+        assertTrue(stats.stream().filter(s->s.getType() == StatType.MIXED_EIGHT).count() == 1);
+        assertTrue(stats.stream().filter(s->s.getType() == StatType.MIXED_NINE).count() == 1);
+        assertTrue(stats.stream().filter(s->s.getType() == StatType.MIXED_SCOTCH).count() == 1);
+        Stat stat = stats.stream().filter(s->s.getType() == StatType.MIXED_EIGHT).findAny().get();
+        assertEquals(new Integer(1),stat.getMatches());
+        assertEquals(new Integer(1),stat.getWins());
+        assertEquals(new Integer(0),stat.getLoses());
+        assertEquals(new Integer(5),stat.getRacksWon());
+        assertEquals(new Integer(1),stat.getRacksLost());
+
+        stat = stats.stream().filter(s->s.getType() == StatType.MIXED_NINE).findAny().get();
+        assertEquals(new Integer(0),stat.getMatches());
+        assertEquals(new Integer(0),stat.getWins());
+        assertEquals(new Integer(0),stat.getLoses());
+        assertEquals(new Integer(0),stat.getRacksWon());
+        assertEquals(new Integer(0),stat.getRacksLost());
+
+
+        stat = stats.stream().filter(s->s.getType() == StatType.MIXED_SCOTCH).findAny().get();
+        assertEquals(new Integer(1),stat.getMatches());
+        assertEquals(new Integer(0),stat.getWins());
+        assertEquals(new Integer(1),stat.getLoses());
+        assertEquals(new Integer(1),stat.getRacksWon());
+        assertEquals(new Integer(5),stat.getRacksLost());
+
+        stat = stats.stream().filter(s->s.getType() == StatType.USER_SEASON).findAny().get();
+        assertEquals(new Integer(2),stat.getMatches());
+        assertEquals(new Integer(1),stat.getWins());
+        assertEquals(new Integer(1),stat.getLoses());
+        assertEquals(new Integer(6),stat.getRacksWon());
+        assertEquals(new Integer(6),stat.getRacksLost());
+
     }
 }
